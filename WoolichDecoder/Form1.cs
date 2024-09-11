627517bfdde5684d77f0bd5bb5ae4413cdcd9151
closed-beta <taktlos@wp.pl>
2024-09-11 19:24:15 +0200

    More GUI

--- WoolichDecoder/Form1.cs
+++ WoolichDecoder/Form1.cs
@@ -1019,6 +1019,39 @@ namespace WoolichDecoder
 
         }
 
+        private void label7_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
